47cb7918d12234a9a0bc525bbc2bb4d882c15cf5
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-07-09 10:53:15 +0300

    pushed new tag

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-tag=1.1.0
-
+tag=1.2.0
+$(aws ecr get-login --no-include-email --profile prod)
 docker build -t 572995054717.dkr.ecr.eu-west-1.amazonaws.com/y2_elastic_fetcher:$tag .
 docker push 572995054717.dkr.ecr.eu-west-1.amazonaws.com/y2_elastic_fetcher:$tag
